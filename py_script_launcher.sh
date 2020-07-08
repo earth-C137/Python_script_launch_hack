e403d464d5926c08a697b3fa419fc678f9f7bebf
earth-C137 <42741144+earth-C137@users.noreply.github.com>
2020-07-07 21:23:05 -0500

    Update py_script_launcher.sh

--- py_script_launcher.sh
+++ py_script_launcher.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-filepath='/home/user/Desktop/tkinter_turbo_calc.py'
-tmux new -d -s p_script
-tmux send-keys -t p_script 'python ' $filepath
-#tmux send-keys -t test 'python /home/user/Desktop/tkinter_turbo_calc.py'
-tmux send-keys -t p_script Enter
-tmux send-keys -t p_script 'exit ' Enter
+#example filepath, replace with your own
+filepath='/home/user/file_path.py'
 
+#starts new detached tmux session named p_script
+tmux new -d -s p_script
+#sends the command to run
+#I use python so it calls conda python, in my case, python3
+#must be in quotes and have space
+tmux send-keys -t p_script 'python ' $filepath
+#sends return to execute the command
+tmux send-keys -t p_script Enter
+#closes tmux once the python script completes
+tmux send-keys -t p_script 'exit ' Enter
+#If python script doesn't complete, then tmux must be closed manually
+#open shell
+#tmux a -t p_script
+#exit
